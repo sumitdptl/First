509295efd5dde03bad1de61b5dccbfdfe33ad5eb
PriyaPatil4801 <108927502+PriyaPatil4801@users.noreply.github.com>
2022-07-08 15:38:27 +0530

    EmployeeDAL
    First time uploaded OMR file by Priya

---
+++ EmployeeDALOMR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using OMRWebApp.Models;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+
+namespace OMRWebApp.DAL
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class EmployeeDALOMR: DbContext
+    {
+        public DbSet<EmployeeBOL> emp { get; set; }
+        public EmployeeDALOMR() : base("WebDataCon")
+        {
+        }
+    }
+}
